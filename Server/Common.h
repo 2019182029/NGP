2ede85f20372f122feb26152791e6d4fe3168548
2019182029_이원준 <S159753S@tukorea.ac.kr>
2024-11-19 11:23:37 +0900

    Common.h 수정

--- Server/Common.h
+++ Server/Common.h
@@ -9,8 +9,8 @@
 #include <stdlib.h> // exit(), ...
 #include <string.h> // strncpy(), ...
 
-#include <array>
-#include <queue>
+#include <array>  // ClientInfoArray, ServerClientArray
+#include <queue>  // ClientServerQueue
 
 #pragma comment(lib, "ws2_32") // ws2_32.lib 링크
 
@@ -20,25 +20,97 @@ private:
 	BYTE state[2];
 
 public:
-	void setValid(bool isValid) { state[0] = (state[0] & 0b01111111) | (isValid << 7); }
-	void setPlayerNumber(int num) { state[0] = (state[0] & 0b10011111) | ((num & 0b11) << 5); }
-	BYTE getValid() const { return (state[0] >> 7) & 0b1; }
-	int getPlayerNumber() const { return (state[0] >> 5) & 0b11; }
+	void SetPosition(float fx, float fy) { x = fx; y = fy; }
+	void SetReadyBit(bool ready) { state[1] = ready ? (state[1] | 0b10000000) : (state[1] & 0b01111111); }
+	void SetStartBit(bool start) { state[1] = start ? (state[1] | 0b01000000) : (state[1] & 0b10111111); }
+	void SetValidBit(bool valid) { state[1] = valid ? (state[1] | 0b00100000) : (state[1] & 0b11011111); }
+	void SetPlayerNumber(int number) { state[1] = ((state[1] & 0b11100111) | (number << 3)); }
+	void SetItemBit(bool item) { state[1] = item ? (state[1] | 0b00000100) : (state[1] & 0b11111011); }
+	void SetAplliedBit(bool applied) { state[1] = applied ? (state[1] | 0b00000010) : (state[1] & 0b11111101); }
+	void SetSurvivingBit(bool surviving) { state[1] = surviving ? (state[1] | 0b00000001) : (state[1] & 0b11111110); }
+	void SetCurrentSurface(int surface) { state[2] = ((state[2] & 0b00111111) | (surface << 6)); }
+	void SetKeyState(int keyState) { state[2] = ((state[2] & 0b11000011) | (keyState << 4)); }
+	void SetSeed(int seed) { state[2] = ((state[2] & 0b11111100) | seed); }
+
+	bool GetReadyBit() { return (state[1] & 0b10000000) >> 7; }
+	bool GetStartBit() { return (state[1] & 0b01000000) >> 6; }
+	bool GetValidBit() { return (state[1] & 0b00100000) >> 5; }
+	int GetPlayerNumber() { return (state[1] & 0b00011000) >> 3; }
+	bool GetItemBit() { return (state[1] & 0b00000100) >> 2; }
+	bool GetAppliedBit() { return (state[1] & 0b00000010) >> 1; }
+	bool GetSurvivingBit() { return state[1] & 0b00000001; }
+	int GetCurrentSurface() { return (state[2] & 0b11000000) >> 6; }
+	int GetKeyState() { return (state[2] & 0b00111100) >> 2; }
+	int GetSeed() { return state[2] & 0b00000011; }
 };
 
-class InfoCheckThreadArg {
+class ThreadArg {
 private:
-	std::array<Packet, 4>* m_CIA;
-	std::queue<Packet>* m_CSQ;
-	std::array<Packet, 4>* m_SCA;
+	SOCKET m_sock;
 
-	HANDLE* m_WriteEvent;
-	CRITICAL_SECTION* m_CS_CSQ;
-	CRITICAL_SECTION* m_CS_SCA;
+	std::array<Packet, 4>* m_ClientInfoArray;
+	std::queue<Packet>* m_ClientServerQueue;
+	std::array<Packet, 4>* m_ServerClientArray;
+
+	HANDLE* m_ClientInfoArray_Event;
+	CRITICAL_SECTION* m_ClientServerQueue_CS;
+	CRITICAL_SECTION* m_ServerClientArray_CS;
+
+	bool* m_isGameStarted;
 
 public:
-	void SetArg(std::array<Packet, 4>* CIA, std::queue<Packet>* CSQ, std::array<Packet, 4>* SCA, HANDLE* WriteEvent, CRITICAL_SECTION* CS_CSQ, CRITICAL_SECTION* CS_SCA) {
-		m_CIA = CIA; m_CSQ = CSQ; m_SCA = SCA;
-		m_WriteEvent = WriteEvent; m_CS_CSQ = CS_CSQ; m_CS_SCA = CS_SCA;
-	};
+	void SetSocket(SOCKET sock) { m_sock = sock; }
+	void SetClientInfoArray(std::array<Packet, 4>* ClientInfoArray) { m_ClientInfoArray = ClientInfoArray; }
+	void SetClientServerQueue(std::queue<Packet>* ClientServerQueue) { m_ClientServerQueue = ClientServerQueue; }
+	void SetServerClientArray(std::array<Packet, 4>* ServerClientArray) { m_ServerClientArray = ServerClientArray; }
+	void SetClientInfoArrayEvent(HANDLE* ClientInfoArray_Event) { m_ClientInfoArray_Event = ClientInfoArray_Event; }
+	void SetClientServerQueueCS(CRITICAL_SECTION* ClientServerQueue_CS) { m_ClientServerQueue_CS = ClientServerQueue_CS; }
+	void SetServerClientArrayCS(CRITICAL_SECTION* ServerClientArray_CS) { m_ServerClientArray_CS = ServerClientArray_CS; }
+	void SetGameStartOrNot(bool* isGameStarted) { m_isGameStarted = isGameStarted; }
+
+	SOCKET GetSocket() { return m_sock; }
+	std::array<Packet, 4>* GetClientInfoArray() { return m_ClientInfoArray; }
+	std::queue<Packet>* GetClientServerQueue() { return m_ClientServerQueue; }
+	std::array<Packet, 4>* GetServerClientArray() { return m_ServerClientArray; }
+	HANDLE* GetClientInfoArrayEvent() { return m_ClientInfoArray_Event; }
+	CRITICAL_SECTION* GetClientServerQueueCS() { return m_ClientServerQueue_CS; }
+	CRITICAL_SECTION* GetServerClientArrayCS() { return m_ServerClientArray_CS; }
+	bool* GetGameStartOrNot() { return m_isGameStarted; }
 };
+
+// 소켓 함수 오류 출력 후 종료
+inline void err_quit(const char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+inline void err_display(const char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// 소켓 함수 오류 출력
+inline void err_display(int errcode) {
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, errcode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[오류] %s\n", (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
