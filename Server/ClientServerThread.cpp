bc553564a8c30c6791411cbee05873f9a69a23d9
JoJinyoung <68494100+Thedume@users.noreply.github.com>
2024-11-22 11:54:20 +0900

    Update ClientServerThread.cpp
    Fix Init & RecvThread to use Common.h
    Add ClientServerThread thread

--- Server/ClientServerThread.cpp
+++ Server/ClientServerThread.cpp
@@ -1,118 +1,112 @@
 #include <iostream>
+#include <queue>
 #include "Common.h"
 
-using namespace std;
+void init(SOCKET s, std::array<Packet, 4>* CIA, std::queue<Packet>* CSQ, CRITICAL_SECTION* CSQ_CS, CRITICAL_SECTION* SCQ_CS) {
+    bool slotFound = false;
+    for (int i = 0; i < 4; ++i) {
+        if (!CIA->at(i).GetValidBit()) {
+            slotFound = true;
 
-// 전역 변수
-std::array<Packet, 4> ClientInfoArray;
-std::queue<Packet> ClientServerQueue;
-std::array<Packet, 4> ServerClientArray;
-bool isGameStart = false;
+            EnterCriticalSection(CSQ_CS);
+            CIA->at(i).SetValidBit(true);
+            CIA->at(i).SetPlayerNumber(i);
+            LeaveCriticalSection(CSQ_CS);
 
-// 클라이언트 스레드에 전달할 데이터 구조체
-struct ThreadParam {
-	SOCKET s;
-	std::array<Packet, 4>* CIA;
-	std::queue<Packet>* CSQ;
-};
+            Packet response;
+            response.SetValidBit(true);
+            response.SetPlayerNumber(i);
+            send(s, (char*)&response, sizeof(response), 0);
 
-int main() {
-	cout << "Server Starting..." << endl;
-	WSADATA wsa;
-	WSAStartup(MAKEWORD(2, 2), &wsa);
+            HANDLE hRecvThread = CreateThread(NULL, 0, RecvThread, CIA, 0, NULL);
+            if (hRecvThread == NULL) {
+                err_display("RecvThread 생성 실패");
+                closesocket(s);
+                return;
+            }
+            CloseHandle(hRecvThread);
+            return;
+        }
+    }
 
-	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	sockaddr_in serverAddr = {};
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(12345);
-	serverAddr.sin_addr.s_addr = INADDR_ANY;
-
-	bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
-	listen(serverSocket, 4);
-
-	while (true) {
-		sockaddr_in clientAddr;
-		int clientSize = sizeof(clientAddr);
-		SOCKET clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientSize);
-		if (clientSocket != INVALID_SOCKET) {
-			cout << "Client Connected" << endl;
-
-			// ClientServerThread 스레드 생성
-			ThreadParam* param = new ThreadParam{ clientSocket, &ClientInfoArray, &ClientServerQueue };
-			CreateThread(NULL, 0, ClientServerThread, param, 0, NULL);
-		}
-	}
-
-	closesocket(serverSocket);
-	WSACleanup();
-	return 0;
-
+    if (!slotFound) {
+        Packet response;
+        response.SetValidBit(false);
+        send(s, (char*)&response, sizeof(response), 0);
+    }
 }
 
-void init(SOCKET s, std::array<Packet, 4>* CIA, std::queue<Packet>* CSQ) {
-	bool assigned = false;
-	for (int i = 0; i < CIA->size(); i++) {
-		if ((*CIA)[i].getValid() == 0) {
-			// 클라이언트 정보를 초기화
-			Packet pkt;
-			pkt.setValid(true);
-			pkt.setPlayerNumber(i);
-			(*CIA)[i] = pkt;
+DWORD WINAPI RecvThread(LPVOID arg) {
+    auto* args = reinterpret_cast<ThreadArg*>(arg);
+    SOCKET s = args->GetSocket();
+    auto* CIA = args->GetClientInfoArray();
+    auto* CSQ = args->GetClientServerQueue();
+    bool* isGameStarted = args->GetGameStartOrNot();
+    HANDLE* CIA_Event = args->GetClientInfoArrayEvent();
+    CRITICAL_SECTION* CSQ_CS = args->GetClientServerQueueCS();
+    CRITICAL_SECTION* SCA_CS = args->GetServerClientArrayCS();
 
-			// 클라이언트에게 응답
-			send(s, reinterpret_cast<const char*>(&pkt), sizeof(pkt), 0);
+    while (true) {
+        Packet receivedPacket;
+        int retval = recv(s, (char*)&receivedPacket, sizeof(receivedPacket), 0);
+        if (retval <= 0) {
+            err_display("recv()");
+            break;
+        }
 
-			// Recv 스레드 생성
-			ThreadParam* param = new ThreadParam{ s, CIA, CSQ };
-			CreateThread(NULL, 0, RecvThread, param, 0, NULL);
-			assigned = true;
-			break;
-		}
-	}
-	if (!assigned) {
-		// 빈 자리가 없는 경우
-		Packet invalidPkt;
-		invalidPkt.setValid(false);
-		send(s, reinterpret_cast<const char*>(&invalidPkt), sizeof(invalidPkt), 0);
-	}
+        if (!*isGameStarted) {
+            EnterCriticalSection(CSQ_CS);
+            int playerNumber = receivedPacket.GetPlayerNumber();
+            if (playerNumber >= 0 && playerNumber < 4) {
+                CIA->at(playerNumber) = receivedPacket;
+            }
+            LeaveCriticalSection(CSQ_CS);
+        }
+        else {
+            EnterCriticalSection(CSQ_CS);
+            CSQ->push(receivedPacket);
+            LeaveCriticalSection(CSQ_CS);
+        }
+    }
+
+    closesocket(s);
+    return 0;
 }
 
-DWORD WINAPI RecvThread(LPVOID param) {
-	ThreadParam* recvParam = reinterpret_cast<ThreadParam*>(param);
-	SOCKET s = recvParam->s;
-	std::array<Packet, 4>* CIA = recvParam->CIA;
-	std::queue<Packet>* CSQ = recvParam->CSQ;
+DWORD WINAPI ClientServerThread(LPVOID arg) {
+    auto* args = reinterpret_cast<ThreadArg*>(arg);
+    SOCKET s = args->GetSocket();
+    auto* CIA = args->GetClientInfoArray();
+    auto* CSQ = args->GetClientServerQueue();
+    auto* SCA = args->GetServerClientArray();
+    bool* isGameStarted = args->GetGameStartOrNot();
+    HANDLE* CIA_Event = args->GetClientInfoArrayEvent();
+    CRITICAL_SECTION* CSQ_CS = args->GetClientServerQueueCS();
+    CRITICAL_SECTION* SCA_CS = args->GetServerClientArrayCS();
 
-	while (true) {
-		Packet receivedPkt;
-		int bytesReceived = recv(s, reinterpret_cast<char*>(&receivedPkt), sizeof(receivedPkt), 0);
-		if (bytesReceived > 0) {
-			if (!isGameStart) {
-				// 게임 시작 전 상태 업데이트
-				int playerNum = receivedPkt.getPlayerNumber();
-				if (playerNum >= 0 && playerNum < CIA->size()) {
-					(*CIA)[playerNum] = receivedPkt;
-				}
-			}
-			else {
-				// 게임 중 데이터 큐에 추가
-				CSQ->push(receivedPkt);
-			}
-		}
-	}
+    init(s, CIA, CSQ, CSQ_CS, SCA_CS);
 
-	delete recvParam; // 동적 메모리 해제
-	return 0;
-}
+    while (true) {
+        if (*isGameStarted) {
+            static int elapsedTime = 0;
+            const int Timeout = 5;
 
-DWORD WINAPI ClientServerThread(LPVOID param) {
-	/*init(s, CIA*, CSQ*);
+            if (elapsedTime > Timeout) {
+                EnterCriticalSection(SCA_CS);
+                for (const auto& packet : *SCA) {
+                    send(s, (char*)&packet, sizeof(packet), 0);
+                }
+                LeaveCriticalSection(SCA_CS);
 
-	while (1) {
-		if (isGameStart) {
-			if (elapsedTime > Timeout) {
-				ServerClientArray의 정보를 Read하여 클라이언트에게 send();
-			}
-		}
-	}*/
+                elapsedTime = 0;
+            }
+
+            elapsedTime++;
+            Sleep(1000);
+        }
+    }
+
+    closesocket(s);
+    delete args;
+    return 0;
 }
