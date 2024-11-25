c5dbdfdafc27d841d2041c5ff1634347a8b234c3
JoJinyoung <68494100+Thedume@users.noreply.github.com>
2024-11-25 19:46:00 +0900

    Fixed ClientServerThread.cpp
    Add CriticalSection & Fixed Time

--- Server/ClientServerThread.cpp
+++ Server/ClientServerThread.cpp
@@ -2,26 +2,39 @@
 #include <queue>
 #include "Common.h"
 
-void init(SOCKET s, std::array<Packet, 4>* CIA, std::queue<Packet>* CSQ, CRITICAL_SECTION* CSQ_CS, CRITICAL_SECTION* SCQ_CS) {
+DWORD WINAPI RecvThread(LPVOID arg);
+
+
+
+void init(SOCKET* s, std::array<Packet, 4>* CIA, std::queue<Packet>* CSQ, HANDLE* CIA_WriteEvent, HANDLE* CIA_ReadEvent, CRITICAL_SECTION* CSQ_CS, CRITICAL_SECTION* SCQ_CS) {
     bool slotFound = false;
+    CRITICAL_SECTION sendCS;
+    WaitForSingleObject(CIA_ReadEvent, INFINITE);
+
     for (int i = 0; i < 4; ++i) {
         if (!CIA->at(i).GetValidBit()) {
             slotFound = true;
 
-            EnterCriticalSection(CSQ_CS);
             CIA->at(i).SetValidBit(true);
             CIA->at(i).SetPlayerNumber(i);
-            LeaveCriticalSection(CSQ_CS);
 
             Packet response;
             response.SetValidBit(true);
             response.SetPlayerNumber(i);
-            send(s, (char*)&response, sizeof(response), 0);
+            //std::cout << response.GetValidBit() << std::endl;
+
+            EnterCriticalSection(&sendCS);
+            int retval = send(*s, (char*)&response, sizeof(response), 0);
+            if (retval <= 0) {
+                err_display("send()");
+                break;
+            }
+            LeaveCriticalSection(&sendCS);
 
             HANDLE hRecvThread = CreateThread(NULL, 0, RecvThread, CIA, 0, NULL);
             if (hRecvThread == NULL) {
                 err_display("RecvThread 생성 실패");
-                closesocket(s);
+                closesocket(*s);
                 return;
             }
             CloseHandle(hRecvThread);
@@ -29,10 +42,14 @@ void init(SOCKET s, std::array<Packet, 4>* CIA, std::queue<Packet>* CSQ, CRITICA
         }
     }
 
+    SetEvent(CIA_WriteEvent);
+
     if (!slotFound) {
+        EnterCriticalSection(&sendCS);
         Packet response;
         response.SetValidBit(false);
-        send(s, (char*)&response, sizeof(response), 0);
+        send(*s, (char*)&response, sizeof(response), 0);
+        LeaveCriticalSection(&sendCS);
     }
 }
 
@@ -42,25 +59,32 @@ DWORD WINAPI RecvThread(LPVOID arg) {
     auto* CIA = args->GetClientInfoArray();
     auto* CSQ = args->GetClientServerQueue();
     bool* isGameStarted = args->GetGameStartOrNot();
-    HANDLE* CIA_Event = args->GetClientInfoArrayEvent();
+    HANDLE* CIA_WriteEvent = args->GetClientInfoArrayWriteEvent();
+    HANDLE* CIA_ReadEvent = args->GetClientInfoArrayReadEvent();
     CRITICAL_SECTION* CSQ_CS = args->GetClientServerQueueCS();
     CRITICAL_SECTION* SCA_CS = args->GetServerClientArrayCS();
+    CRITICAL_SECTION recvCS;
 
     while (true) {
+
+        EnterCriticalSection(&recvCS);
         Packet receivedPacket;
         int retval = recv(s, (char*)&receivedPacket, sizeof(receivedPacket), 0);
+        LeaveCriticalSection(&recvCS);
+
         if (retval <= 0) {
+            std::cout << WSAGetLastError() << std::endl;
             err_display("recv()");
             break;
         }
-
+        //std::cout << "???";
         if (!*isGameStarted) {
-            EnterCriticalSection(CSQ_CS);
+            WaitForSingleObject(CIA_ReadEvent, INFINITE);  // 시간 설정 필요
             int playerNumber = receivedPacket.GetPlayerNumber();
             if (playerNumber >= 0 && playerNumber < 4) {
                 CIA->at(playerNumber) = receivedPacket;
             }
-            LeaveCriticalSection(CSQ_CS);
+            SetEvent(CIA_WriteEvent);
         }
         else {
             EnterCriticalSection(CSQ_CS);
@@ -80,31 +104,42 @@ DWORD WINAPI ClientServerThread(LPVOID arg) {
     auto* CSQ = args->GetClientServerQueue();
     auto* SCA = args->GetServerClientArray();
     bool* isGameStarted = args->GetGameStartOrNot();
-    HANDLE* CIA_Event = args->GetClientInfoArrayEvent();
+    HANDLE* CIA_WriteEvent = args->GetClientInfoArrayWriteEvent();
+    HANDLE* CIA_ReadEvent = args->GetClientInfoArrayReadEvent();
     CRITICAL_SECTION* CSQ_CS = args->GetClientServerQueueCS();
     CRITICAL_SECTION* SCA_CS = args->GetServerClientArrayCS();
 
-    init(s, CIA, CSQ, CSQ_CS, SCA_CS);
+    
+
+    init(&s, CIA, CSQ, CIA_WriteEvent, CIA_ReadEvent, CSQ_CS, SCA_CS);
 
     while (true) {
         if (*isGameStarted) {
-            static int elapsedTime = 0;
-            const int Timeout = 5;
+            auto beforeTime = std::chrono::high_resolution_clock::now();
+            auto currentTime = std::chrono::high_resolution_clock::now();
+            auto elapsedTime = std::chrono::microseconds(0);
+            auto totalElapsedTime = std::chrono::microseconds(0);
 
-            if (elapsedTime > Timeout) {
+            int Timeout = 3;
+
+            if (elapsedTime.count() > Timeout) {
                 EnterCriticalSection(SCA_CS);
                 for (const auto& packet : *SCA) {
                     send(s, (char*)&packet, sizeof(packet), 0);
                 }
                 LeaveCriticalSection(SCA_CS);
-
-                elapsedTime = 0;
             }
 
-            elapsedTime++;
+            currentTime = std::chrono::high_resolution_clock::now();
+            elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(currentTime - beforeTime);
+            totalElapsedTime += std::chrono::duration_cast<std::chrono::microseconds>(currentTime - beforeTime);
+            beforeTime = std::chrono::high_resolution_clock::now();
+
             Sleep(1000);
         }
     }
+
+    
 
     closesocket(s);
     delete args;
