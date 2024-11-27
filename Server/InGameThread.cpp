812bf09672d04359ecf8366446a5823917f7f6a8
2019182029_이원준 <S159753S@tukorea.ac.kr>
2024-11-27 16:55:35 +0900

    InGameThread.cpp 수정
    -InGameThread.cpp
    기존 반복문의 코드를 PopFromClientServerQueue(), Update(), RenewalServerClientArray() 함수로 분리
    기존의 ClientInfo를 Packet을 상속받는 Object 배열로 수정, xdir과 ydir 멤버 변수를 추가로 가지며 Update() 함수에서 갱신

--- Server/InGameThread.cpp
+++ Server/InGameThread.cpp
@@ -1,7 +1,7 @@
 #include "Common.h"
 #include "InGameThread.h"
 
-void Init(std::array<Packet, 4>* ClientInfo, std::array<Packet, 4>* ClientInfoArray, std::array<Packet, 4>* ServerClientArray, CRITICAL_SECTION* ServerClientArray_CS, bool* isGameStart) {
+void Init(std::array<Packet, 4>* ClientInfoArray, std::array<Packet, 4>* ServerClientArray, std::array<Object, 4>* ClientInfo, CRITICAL_SECTION* ServerClientArray_CS, volatile bool* isGameStart) {
 	EnterCriticalSection(ServerClientArray_CS);
 	
 	for (int i = 0; i < 4; ++i) {
@@ -49,74 +49,103 @@ void Init(std::array<Packet, 4>* ClientInfo, std::array<Packet, 4>* ClientInfoAr
 	std::cout << "게임 시작" << std::endl;
 }
 
+void PopFromClientServerQueue(std::queue<Packet>* ClientServerQueue, std::array<Object, 4>* ClientInfo, CRITICAL_SECTION* ClientServerQueue_CS) {
+	if (!ClientServerQueue->empty()) {
+		EnterCriticalSection(ClientServerQueue_CS);
+
+		Update(ClientServerQueue->front(), ClientInfo);  // 큐의 첫 번째 요소를 가져와서 플레이어 정보 갱신
+		ClientServerQueue->pop();  // 큐의 첫 번째 요소 삭제
+
+		LeaveCriticalSection(ClientServerQueue_CS);
+	}
+}
+
+void Update(Packet packet, std::array<Object, 4>* ClientInfo) {
+	switch (packet.GetKeyState() ^ (*ClientInfo)[packet.GetPlayerNumber()].GetKeyState()) {
+	case 0b0100:  // A 키
+		if (packet.GetKeyState() & 0100) { (*ClientInfo)[packet.GetPlayerNumber()].SetDir(-1.0f, 0.0f); }
+		else { (*ClientInfo)[packet.GetPlayerNumber()].SetDir(1.0f, 0.0f); }
+		break;
+
+	case 0b0010:  // D 키
+		if (packet.GetKeyState() & 0010) { (*ClientInfo)[packet.GetPlayerNumber()].SetDir(1.0f, 0.0f); }
+		else { (*ClientInfo)[packet.GetPlayerNumber()].SetDir(-1.0f, 0.0f); }
+		break;
+
+	case 0b0001:  // C 키
+		if (packet.GetKeyState() & 0001) {  
+			if ((*ClientInfo)[packet.GetPlayerNumber()].GetItemBit()) {  // 해당 플레이어가 아이템을 가지고 있다면
+				for (int i = 0; i < 4; ++i) {
+					if (i != packet.GetPlayerNumber()) {  // 해당 플레이어를 제외한 나머지 모든 플레이어에게
+						(*ClientInfo)[i].SetAplliedBit(true);  // 아이템 효과 적용 후
+					}
+				}
+				(*ClientInfo)[packet.GetPlayerNumber()].SetItemBit(0);  // 해당 플레이어의 아이템 보유 여부를 false로 갱신
+			}
+		}
+		break;
+
+	default: 
+		break;
+	}
+
+	(*ClientInfo)[packet.GetPlayerNumber()].SetKeyState(packet.GetKeyState());  // ClientInfo의 KeyState 갱신
+}
+
+void RenewalServerClientArray(std::array<Packet, 4>* ServerClientArray, std::array<Object, 4>* ClientInfo, CRITICAL_SECTION* ServerClientArray_CS) {
+	EnterCriticalSection(ServerClientArray_CS);
+
+	for (int i = 0; i < 4; ++i) {
+		if ((*ClientInfo)[i].GetValidBit()) {
+			(*ServerClientArray)[i].SetItemBit((*ClientInfo)[i].GetItemBit());
+			(*ServerClientArray)[i].SetAplliedBit((*ClientInfo)[i].GetAppliedBit());
+			(*ServerClientArray)[i].SetSurvivingBit((*ClientInfo)[i].GetSurvivingBit());
+			(*ServerClientArray)[i].SetPosition((*ClientInfo)[i].GetXPosition(), (*ClientInfo)[i].GetYPosition());
+			(*ServerClientArray)[i].SetCurrentSurface((*ClientInfo)[i].GetCurrentSurface());
+		}
+	}
+
+	LeaveCriticalSection(ServerClientArray_CS);
+}
+
 DWORD __stdcall InGameThread(LPVOID arg) {
-	std::array<Packet, 4> ClientInfo;
+	std::array<Object, 4> Players;
+	std::array<Object, 10> Obstacles;
 
 	auto beforeTime = std::chrono::high_resolution_clock::now();
 	auto currentTime = std::chrono::high_resolution_clock::now();
-	auto elapsedTime = std::chrono::microseconds(0);
+	double elapsedTime;
 	auto totalElapsedTime = std::chrono::microseconds(0);
 
-	Init(&ClientInfo, ((ThreadArg*)arg)->GetClientInfoArray(), ((ThreadArg*)arg)->GetServerClientArray(), ((ThreadArg*)arg)->GetServerClientArrayCS(), ((ThreadArg*)arg)->GetGameStartOrNot());
+	Init(((ThreadArg*)arg)->GetClientInfoArray(), ((ThreadArg*)arg)->GetServerClientArray(), &Players, ((ThreadArg*)arg)->GetServerClientArrayCS(), ((ThreadArg*)arg)->GetGameStartOrNot());
 
 	// 게임 루프
 	while (1) {
 		// ClientServerQueue가 비어있지 않다면 ClientInfo 갱신
-		if (!((ThreadArg*)arg)->GetClientServerQueue()->empty()) {  
-			EnterCriticalSection(((ThreadArg*)arg)->GetClientServerQueueCS());
-
-			Packet p = ((ThreadArg*)arg)->GetClientServerQueue()->front();  // 큐의 첫 번째 요소 가져오기
-
-			if (p.GetKeyState() & 0001) {  // C 키가 눌린 거라면
-				if (ClientInfo[p.GetPlayerNumber()].GetItemBit()) {  // 해당 플레이어가 아이템을 가지고 있다면
-					for (int i = 0; i < 4; ++i) {
-						if (i != p.GetPlayerNumber()) {  // 해당 플레이어를 제외한 나머지 모든 플레이어에게
-							ClientInfo[i].SetAplliedBit(true);  // 아이템 적용
-						}
-					}
-				}
-			}
-			ClientInfo[p.GetPlayerNumber()].SetKeyState(p.GetKeyState());  // ClientInfo 갱신
-
-			((ThreadArg*)arg)->GetClientServerQueue()->pop();  // 큐의 첫 번째 요소 삭제
-
-			LeaveCriticalSection(((ThreadArg*)arg)->GetClientServerQueueCS());
-		}
+		PopFromClientServerQueue(((ThreadArg*)arg)->GetClientServerQueue(), &Players, ((ThreadArg*)arg)->GetClientServerQueueCS());
 
 		// 모든 플레이어가 사망했다면 프로그램 종료
-		if (!std::count_if(ClientInfo.begin(), ClientInfo.end(), [](auto& packet) { return packet.GetSurvivingBit() == 0; })) {  
+		if (!std::count_if(Players.begin(), Players.end(), [](auto& packet) { return packet.GetSurvivingBit() == 0; })) {
 			((ThreadArg*)arg)->SetGameStartOrNot(false);
 			break;
 		}
 
 		currentTime = std::chrono::high_resolution_clock::now();
-		elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(currentTime - beforeTime);
+		elapsedTime = ((std::chrono::duration<double>)std::chrono::duration_cast<std::chrono::microseconds>(currentTime - beforeTime)).count();
 		totalElapsedTime += std::chrono::duration_cast<std::chrono::microseconds>(currentTime - beforeTime);
 		beforeTime = std::chrono::high_resolution_clock::now();
 
 		// 클라이언트 이동, 충돌 검사
-		for (auto& packet : ClientInfo) {
-			// 클리아인트 이동 : Position, CurrentSurface
-			// 충돌 검사 : Item, Applied, Surviving
+		for (auto& ObjectInfo : Players) {
+			// 클리아인트 이동 
+
+
+			// 충돌 검사 : Item, Surviving
 		}
 
 		// totalElapsedTime이 60분의 1초를 경과했을 시 ServerClientArray 갱신
-		if (elapsedTime.count() >= 16'667) { 
-			std::array<Packet, 4>* ServerClientArray = ((ThreadArg*)arg)->GetServerClientArray();
-
-			EnterCriticalSection(((ThreadArg*)arg)->GetServerClientArrayCS());
-				
-			for (int i = 0; i < 4; ++i) {
-				if (ClientInfo[i].GetValidBit()) {
-					(*ServerClientArray)[i].SetItemBit(ClientInfo[i].GetItemBit());
-					(*ServerClientArray)[i].SetAplliedBit(ClientInfo[i].GetAppliedBit());
-					(*ServerClientArray)[i].SetSurvivingBit(ClientInfo[i].GetSurvivingBit());
-					(*ServerClientArray)[i].SetPosition(ClientInfo[i].GetXPosition(), ClientInfo[i].GetYPosition());
-					(*ServerClientArray)[i].SetCurrentSurface(ClientInfo[i].GetCurrentSurface());
-				}
-			}
-
-			LeaveCriticalSection(((ThreadArg*)arg)->GetServerClientArrayCS());
+		if (totalElapsedTime.count() >= 16'667) {
+			RenewalServerClientArray(((ThreadArg*)arg)->GetServerClientArray(), &Players, ((ThreadArg*)arg)->GetServerClientArrayCS());
 
 			totalElapsedTime = std::chrono::microseconds(0);  // totalElapsedTime를 0초로 갱신
 		}
--- Server/InGameThread.h
+++ Server/InGameThread.h
@@ -1,4 +1,19 @@
 #pragma once
 
-void Init(std::array<Packet, 4>* ClientInfo, std::array<Packet, 4>* ClientInfoArray, std::array<Packet, 4>* ServerClientArray, CRITICAL_SECTION* ServerClientArray_CS, bool* isGameStart);
+class Object : public Packet {
+private:
+	float xdir, ydir;
+
+public:
+	void SetDir(float fxdir, float fydir) { xdir += fxdir; ydir += fydir; }
+
+	float GetXDir() { return xdir; }
+	float GetYDir() { return ydir; }
+};
+
+void Init(std::array<Packet, 4>* ClientInfoArray, std::array<Packet, 4>* ServerClientArray, std::array<Object, 4>* ClientInfo, CRITICAL_SECTION* ServerClientArray_CS, volatile bool* isGameStart);
+void PopFromClientServerQueue(std::queue<Packet>* ClientServerQueue, std::array<Object, 4>* ClientInfo, CRITICAL_SECTION* ClientServerQueue_CS);
+void Update(Packet packet, std::array<Object, 4>* ClientInfo);
+void RenewalServerClientArray(std::array<Packet, 4>* ServerClientArray, std::array<Object, 4>* ClientInfo, CRITICAL_SECTION* ServerClientArray_CS);
+
 DWORD WINAPI InGameThread(LPVOID arg);
